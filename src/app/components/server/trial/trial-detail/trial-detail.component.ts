3206b50c83d2fad50bebd6eb76936b85ffe25a3c
Uixes <c6185886@urhen.com>
2020-06-05 02:03:32 +0300

    exactly not final version here

---
+++ src/app/components/server/trial/trial-detail/trial-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trial-detail',
+  templateUrl: './trial-detail.component.html',
+  styleUrls: ['./trial-detail.component.css']
+})
+export class TrialDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
